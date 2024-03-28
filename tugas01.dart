58eb132c4a3592999ea0b8b04e412f0c3c37033a
22410100067Evan <164966886+22410100067Evan@users.noreply.github.com>
2024-03-28 08:43:59 +0700

    Update tugas01.dart

--- tugas01.dart
+++ tugas01.dart
@@ -1,11 +1,11 @@
 void main() {
-var n = 87;
-  if(n<=50 ){
+var n = 79.6;
+  if(n<=50.9 ){
     print("D");
-  }else if(n>=51 && n<=67){
+  }else if(n>=51 && n<=67.9){
    print("C");
     
-  }else if(n>=68 && n<=79){
+  }else if(n>=68 && n<=79.9){
    print("B");
     
   }else if(n>=80 && n<=100){
