9d789a4e48ec9ea461b8e8fb93b2f2fbec634b47
22410100067Evan <164966886+22410100067Evan@users.noreply.github.com>
2024-04-17 23:12:02 +0700

    Update tugas12.dart

--- Pertemuan 3/Tugas/tugas12.dart
+++ Pertemuan 3/Tugas/tugas12.dart
@@ -33,9 +33,9 @@ class Karyawan{
 }
 
 void main(){
-  Karyawan x = Karyawan("Rommy", "Data Analyst", "40000");
+  Karyawan x = Karyawan("Chris", "Web Developer", "75000");
   x.Detailkaryawan();
-  x.gaji = "50000";
-  x.jabatan = "Developer";
+  x.gaji = "60000";
+  x.jabatan = "Team Manager";
   x.Detailkaryawan();
 }
